2fe20c5616d7fe3145d7d2a9e54ef4150ab46d51
larisillvs <oliveira.larissa26@escola.pr.gov.br>
2025-04-15 08:19:53 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,39 @@
+function setup() {
+  createCanvas(400, 400);
+  
+  x = random(400);  
+  y = random(400);
+  x = int(x);
+  y = int(y);
+}
+
+function draw() {
+  background(220);
+  
+  x = x + random (-5, 5);
+  y = y + random (-5, 5);
+  
+  // delimitar para não sair da tela
+  x = constrain(x, 0, 400);
+  y = constrain(y, 0, 400);
+  
+ //circle(x, y, 10);
+  let distanciaX; let distanciaY; let distancia;
+  distanciaX = mouseX - x;
+  distanciaY = mouseY - y;
+  
+  //teorema de pitagoras - trigonometria
+  
+  distancia = sqrt(distanciaX * distanciaX + distanciaY * distanciaY);
+  
+  console.log(distancia); 
+  circle(mouseX, mouseY, distancia)
+  
+  
+  if( distancia < 3){
+   text('Encontrei', 200, 200);
+    noLoop();
+     }
+  
+  
+}
